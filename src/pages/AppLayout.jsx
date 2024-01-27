88e2c2e06498a46bfa8694e022dc86269f4060d9
Khushal Mali <khushalmali8448@gmail.com>
2024-01-28 00:50:40 +0530

    react app

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,18 @@
+import AppNav from "../components/AppNav";
+import Map from "../components/Map";
+import User from "../components/User";
+import SideBar from "../components/SideBar";
+
+import styles from "./AppLayout.module.css";
+
+function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <SideBar />
+      <Map />
+      <User />
+    </div>
+  );
+}
+
+export default AppLayout;
